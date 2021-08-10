8e01585e7de6a978a3862a881ea0f1b8a6bce1dd
sercangunonu <sercan@berenbilisim.com>
2021-08-10 17:50:01 +0300

    Initial Commit

---
+++ ScorpCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScorpCase
+//
+//  Created by sercan günönü on 10.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
